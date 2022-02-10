d6a2fa5132867174fb3880f1aa937c47c5b4cde5
mprat21 <pmpratikmore1@gmail.com>
2022-02-10 16:14:28 +0100

    l

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,19 +74,8 @@ const config = {
           },
         ],
       },
-      
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
-      <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js">
-      </script>
-      <script type="text/javascript">
-        algoliasearchNetlify({
-          appId: '5XMQ2C4NAT',
-          apiKey: '96440888ab5870fd4f0587db05468243',
-          siteId: 'e1a12213-e46b-4768-ad8f-237eaf256b48',
-          branch: 'master-main',
-          selector: 'div#search',
-        });
-      </script>
+
+
     /* algolia: {
         // Public API key: it is safe to commit it
         apiKey: '96440888ab5870fd4f0587db05468243',
@@ -94,6 +83,15 @@ const config = {
         appId: '5XMQ2C4NAT'
       },
       */
+      algoliasearchNetlify:
+      {
+    appId: '5XMQ2C4NAT',
+    apiKey: '96440888ab5870fd4f0587db05468243',
+    siteId: 'e1a12213-e46b-4768-ad8f-237eaf256b48',
+    branch: 'master-main',
+    selector: 'div#search',
+  },
+
 
       footer: {
         style: 'dark',
